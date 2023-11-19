3e56edf78bfe0463d4ee79cb0fbc6beafdb4a941
Sruthy-Sreekumar <148990178+Sruthy-Sreekumar@users.noreply.github.com>
2023-11-19 20:36:02 +0530

    largestnumber.py

---
+++ largest.py
@@ -0,0 +1,9 @@
+a=int(input("Enter the first number:"))
+b=int(input("Enter the second number:"))
+c=int(input("Enter the third number:")) 
+if a>b and a>c:
+  print ("largest number is:",a)
+elif b>a and b>c:
+ print("largest number is:",b) 
+else:
+ print("largest number is:",c)
