767b312da9c3e0b38167ba47ff125e559fd62e33
Sruthy-Sreekumar <148990178+Sruthy-Sreekumar@users.noreply.github.com>
2023-11-19 21:24:40 +0530

    studentlistusingdictionary.py

---
+++ dictoinary.py
@@ -0,0 +1,8 @@
+student={}
+n=int(input("enter the number of students:"))
+for i in range(0,n):
+ name=input("enter the name:")
+ age=int(input("enter the age:"))
+ grade=input("enter the grade:")
+ student[name]=[age,grade]
+ print("list of students:",student)
